48e2bc9e7c896196c669790baa6352b3023b1481
Karner David <viEclipsekd@gmail.com>
2024-09-08 18:46:59 -0500

    paired 2D images with 3D model with script

--- script_pair_2D_3D.py
+++ script_pair_2D_3D.py
@@ -1,17 +1,29 @@
 import json
 import os
 
-# Load the metadata JSON file from the Pix3D dataset
-with open("pix3d.json", 'r') as f:
-    data = json.load(f)
+with open('pix3d.json', 'r') as f:
+    metadata = json.load(f)
 
-img_model_pairs = {}
+resized_img_head = '../resized_images'
+model_head = '../model'
 
-for item in data:
-    img_path = item['img']
-    model_path = item['model']
+image_to_model_map = {}
 
-    img_model_pairs[img_path] = model_path
+for item in metadata:
+    orig_img_path = item['img'].replace('img/', '')
+    img_full_path = os.path.join(resized_img_head, orig_img_path)
 
-for img, model in list(img_model_pairs.items()):
-    print(f"Image: {img} -> Model: {model}")
+    model_path = item['model'].replace('model/', '')
+    model_full_path = os.path.join(model_head, model_path)
+
+    model_dir = os.path.dirname(model_full_path)
+    processed_model_path = os.path.join(model_dir, 'simple_normal_model.obj')
+
+    if os.path.exists(img_full_path) and os.path.exists(processed_model_path):
+        # save pair into map
+        image_to_model_map[img_full_path] = processed_model_path
+
+with open('resized_img_processed_model_mapping.json', 'w') as outfile:
+    json.dump(image_to_model_map, outfile, indent=4)
+
+print(f'Mapping saved to resized_image_processed_model_mapping.json')
